8ed0eabb486a0190c00373213b3765e0e44e82bb
widehrt3-hub <widehrt3@gmail.com>
2026-01-06 14:20:09 +0900

    Update operators.py

--- day_3/operators.py
+++ day_3/operators.py
@@ -1,1 +1,21 @@
+age = 21
 
+height = 177.9
+
+complex_num = 1 + 2j
+
+base = float(input("Enter base :"))
+height = float(input("Enter height :"))
+area = 0.5 * base * height
+print("The area of the triagle is", area)
+
+side_a = float(input("Enter side a :"))
+side_b = float(input("Enter side b :"))
+side_c = float(input("Enter side c :"))
+perimeter = side_a + side_b + side_c
+print("The perimeter of the triangle is", perimeter)
+
+
+
+
+# 1. the role of a comma in the function print : to change value into string and add a blank between. => print("The area of the triagle is", area)
